25a012b637b385b0b9c790cce41c25b012af42e0
= <=>
2017-11-26 12:17:54 +0100

    init commit

---
+++ src/main/java/sep/tim6/Tim6Application.java
@@ -0,0 +1,12 @@
+package sep.tim6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tim6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tim6Application.class, args);
+	}
+}
